ee60c0c2f1af1bf6ffe7024664055e1e8fb9993a
Fergut11 <fergut11@mail.lcc.edu>
2025-11-20 08:10:36 -0500

    Update assignment8.py

--- assignment8.py
+++ assignment8.py
@@ -1,6 +1,7 @@
 def main():
     print("This is the main function")
     print(parse_input())
+    print("This is a test for git")
 
 def parse_input():
     user_input = input("Input either a hex or decimal value here")

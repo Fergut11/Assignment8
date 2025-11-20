72f57c90dc005dce74f19c4e90e62125c7180a8c
Fergut11 <fergut11@mail.lcc.edu>
2025-11-20 09:09:14 -0500

    Update assignment8.py
    added comments on everything that needs them I think.

--- assignment8.py
+++ assignment8.py
@@ -4,9 +4,16 @@ def main():
         "Hex values should include either 0x or # at the beginning.\n"
         "Decimal numbers can be input regularly.\n"
     )
+
     print(intro)
+
     while True:
+        # This will run the parse_input() function 
+        # and set the return values to result_type and result_value
         result_type, result_value = parse_input()
+
+        # If the result is invalid and the input was not correct
+        # the user will be asked for another input
         if result_type != "invalid":
             print(f"\nYour original value was {result_type} and converts {result_value} in Binary. ")
             break
@@ -15,28 +22,35 @@ def main():
 
 
 def parse_input():
+    # Gets the user's input. It should be either hex or decimal
     user_input = input("Input your number here : ")
+
+    # The if statement is checking if the input is hexidecimal or not
+    # hex should start with either "0x" or "#"
     if user_input.lower().startswith("0x") or user_input.startswith("#"):
+        # will call the function that converts the actual inputs, in this case hex is True
         return(convert_inputs(user_input, True))
 
     else:
         return(convert_inputs(user_input, False))
 
 def convert_inputs(input_to_convert, is_hex):
+    # When this function is called it is passed either True or False for is_hex
     if is_hex:
-        try:
-            converted_input = bin(int(input_to_convert, 16))[2:]
-            return ("Hex to Binary", converted_input)
-        except ValueError:
+        try: # if unable to convert the input to binary it will throw an exception
+            converted_input = bin(int(input_to_convert, 16))[2:] # the [2:] removes the 0b python adds
+            return ("Hexidecimal", converted_input)
+        except ValueError: # this is the exception, will force user to input another value
             converted_input = "invalid"
             return("invalid", converted_input)
+
     else:
         try:
             converted_input = bin(int(input_to_convert, 10))[2:]
-            return("Decimal to Binary", converted_input)
+            return("Decimal", converted_input)
         except ValueError:
             converted_input = "invalid"
             return("invalid", converted_input) 
 
-
+# this will simply run the main() function
 main()

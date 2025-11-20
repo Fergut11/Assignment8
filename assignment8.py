fd17f3243c3c8e9460707b65ce3734ca2a9ad78c
Fergut11 <fergut11@mail.lcc.edu>
2025-11-20 08:25:54 -0500

    Update assignment8.py
    returns the type of value input, got rid of the test for git part

--- assignment8.py
+++ assignment8.py
@@ -1,13 +1,22 @@
 def main():
     print("This is the main function")
     print(parse_input())
-    print("This is a test for git")
 
 def parse_input():
     user_input = input("Input either a hex or decimal value here")
-    if user_input.lower().startswith("0x"):
-        return (int(user_input, 16))
-    else:
-        return(int(user_input))
+    if user_input.lower().startswith("0x") or user_input.startswith("#"):
+        try:
+            converted_input = int(user_input, 16)
+            return ("hex", converted_input)
+        except ValueError:
+            converted_input = "invalid"
+            return("invalid", converted_input)
+
+    try:
+        converted_input = int(user_input, 10)
+        return("decimal", converted_input)
+    except ValueError:
+        converted_input = "invalid"
+        return("invalid", converted_input) 
 
 main()

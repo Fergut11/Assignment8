46165922f047d63d348e0b04f01bc7103d2ddd1b
Fergut11 <fergut11@mail.lcc.edu>
2025-11-20 08:47:32 -0500

    Update assignment8.py
    Added a new function. I think it obfuscates the code compared to what I had but the assignment calls for 3 functions

--- assignment8.py
+++ assignment8.py
@@ -5,18 +5,26 @@ def main():
 def parse_input():
     user_input = input("Input either a hex or decimal value here")
     if user_input.lower().startswith("0x") or user_input.startswith("#"):
+        return(convert_inputs(user_input, True))
+
+    else:
+        return(convert_inputs(user_input, False))
+
+def convert_inputs(input_to_convert, is_hex):
+    if is_hex:
         try:
-            converted_input = bin(int(user_input, 16))
+            converted_input = bin(int(input_to_convert, 16))[2:]
             return ("hex", converted_input)
         except ValueError:
             converted_input = "invalid"
             return("invalid", converted_input)
+    else:
+        try:
+            converted_input = bin(int(input_to_convert, 10))[2:]
+            return("decimal", converted_input)
+        except ValueError:
+            converted_input = "invalid"
+            return("invalid", converted_input) 
 
-    try:
-        converted_input = bin(int(user_input, 10))
-        return("decimal", converted_input)
-    except ValueError:
-        converted_input = "invalid"
-        return("invalid", converted_input) 
 
 main()

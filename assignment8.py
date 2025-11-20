4dea5e5574df608d3dbd3be79623f24948b94d6c
Fergut11 <fergut11@mail.lcc.edu>
2025-11-20 08:09:39 -0500

    Create assignment8.py

---
+++ assignment8.py
@@ -0,0 +1,12 @@
+def main():
+    print("This is the main function")
+    print(parse_input())
+
+def parse_input():
+    user_input = input("Input either a hex or decimal value here")
+    if user_input.lower().startswith("0x"):
+        return (int(user_input, 16))
+    else:
+        return(int(user_input))
+
+main()

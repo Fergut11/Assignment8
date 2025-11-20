28cdc8e07648d994a53db60ff573081d14294b24
Fergut11 <fergut11@mail.lcc.edu>
2025-11-20 08:33:23 -0500

    Update assignment8.py
    converts the outputs to binary as required

--- assignment8.py
+++ assignment8.py
@@ -6,14 +6,14 @@ def parse_input():
     user_input = input("Input either a hex or decimal value here")
     if user_input.lower().startswith("0x") or user_input.startswith("#"):
         try:
-            converted_input = int(user_input, 16)
+            converted_input = bin(int(user_input, 16))
             return ("hex", converted_input)
         except ValueError:
             converted_input = "invalid"
             return("invalid", converted_input)
 
     try:
-        converted_input = int(user_input, 10)
+        converted_input = bin(int(user_input, 10))
         return("decimal", converted_input)
     except ValueError:
         converted_input = "invalid"
